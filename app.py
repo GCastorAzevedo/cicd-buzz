41425198a7f7f8b83ab973ee537d661470e2b0fc
Gabriel Castor <gabriel.azevedo@b2wdigital.com>
2018-12-06 18:49:07 -0200

    Add .pylintrc

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
+"""CI/CD Buzz Generator App"""
+
 import os
 import signal
 from flask import Flask
 from buzz import generator
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
+#pylint: disable=protected-access
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
-@app.route("/")
+@APP.route("/")
 def generate_buzz():
+    """
+    wrapper for generate_buzz to display the phrases!
+    """
     page = '<html><body><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    APP.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
